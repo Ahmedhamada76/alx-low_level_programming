7985fb9a944b1d88a821f57ce588bd32a809950a
ahmedhamada76 <ahmedfadili76@gmail.com>
2023-03-23 14:37:39 -0700

    creat Write a program that prints _putchar

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,21 +1,10 @@
 #include "main.h"
-/**
- * main - print _putchar
- * Return: 0
- */
-
-
-int main(void)
-{
-    _putchar('a');
-    _putchar('r');
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('\n');
-    return (0);
-}
+	#include <unistd.h>
+	/**
+	 * _putchar - writes the character c to stdout
+	 */
+	char _putchar(char c)
+	{
+		return (write(1, &c, 1));
+	}
 
