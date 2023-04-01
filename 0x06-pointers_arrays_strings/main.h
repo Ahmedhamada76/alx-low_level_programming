72d00c9f8a3927c6a15cd546268ef5916b1c51a7
ahmedhamada76 <ahmedfadili76@gmail.com>
2023-03-31 19:44:03 -0700

    task  7-leet.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,6 +9,7 @@ char *_strncpy(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
-
+char *leet(char *n);
+char *rot13(char *n);
 
 #endif
