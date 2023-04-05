b19c9d8cd6f661bc7e446042ddf151130171df83
ahmedhamada76 <ahmedfadili76@gmail.com>
2023-04-04 19:04:16 -0700

    ./4-strpbrk

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,5 +5,11 @@ int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+char *_strpbrk(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+
+
+
 
 #endif /* main_h */
